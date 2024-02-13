7a2243ceb18b4eb5c43bed4ff75fdd4a108e299b
neerajaabhyankar@Neerajas-MacBook-Air.local Neeraja Abhyankar <neeraja.abhyankar@gmail.com>
2024-02-12 21:43:51 -0800

    chore: format

--- odavmoorchhana.py
+++ odavmoorchhana.py
@@ -1,34 +1,44 @@
 # Author: Neeraja Abhyankar
 # Created: 22 May 2021
 
-#%% IMPORTS
+# %% IMPORTS
 
 import os
 import time
 import numpy as np
 
-#%% PROBLEM DEFINITION
+# %% PROBLEM DEFINITION
 
 """
 I want to write a program that characterizes moorchhana-groups of ragas completely.
 
 """
 
-#%% DATA STRUCTURES
+# %% DATA STRUCTURES
 
 # saptak
 
-swar = ["sa", "komal-re", "re", "komal-ga", "ga", "ma", "teevra-ma", ]
-chakra_5ths = ["sa", "pa", "re",   "ma"]
+swar = [
+    "sa",
+    "komal-re",
+    "re",
+    "komal-ga",
+    "ga",
+    "ma",
+    "teevra-ma",
+]
+chakra_5ths = ["sa", "pa", "re", "ma"]
 # TODO: Make this data structure cyclic
 
 # ODAV raag niyam aani ODAV raag
 
-forbidden_pairs = [("komal-re", "re"), ]
+forbidden_pairs = [
+    ("komal-re", "re"),
+]
 
 # moorchhana operations
 
-# TODO: "moorgat" = a list of "cycles"; 
+# TODO: "moorgat" = a list of "cycles";
 # "cycle" = a cyclic set of 5 raags, some valid some invalid, with one primal raag, lexically determined.
 
-#%%
+# %%
